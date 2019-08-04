d402941b286454038313eda4ef3f1d3048382971
Suyog Ahuja <ahujasuyog1@gmail.com>
2019-08-05 02:56:07 +0530

    2nd challenge updated

--- 2nd_challenge.py
+++ 2nd_challenge.py
@@ -10,201 +10,21 @@ Every year has 365 days
 x = int(input("Enter day"))
 y = int(input("Enter month"))
 z = int(input("Enter year"))
-r1 = x
-
-if y == 1 :
-    r1 = x % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 2:
-    r1 = (r1+3)%7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-
-elif y == 3:
-    r1 = (r1+6)%7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-
-elif y == 4:
-    r1 = (x+2) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 5:
-    r1 = (x + 5) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 6:
-    r1 = (r1 + 6)%7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 7:
-    r1 = (x + 2) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 8:
-    r1 = (x + 5) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 9:
-    r1 = (r1 + 6) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-
-elif y == 10:
-
-    r1 = (x + 2) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 11:
-    r1 = (x + 5) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
-elif y == 12:
-    r1 = (r1 + 6) % 7
-    if r1 == 1:
-        print("Its monday-0")
-    elif r1 == 2:
-        print("Its tuesday-1")
-    elif r1 == 3:
-        print("Its wednesday-2")
-    elif r1 == 4:
-        print("Its thursday-3")
-    elif r1 == 5:
-        print("Its Friday-4")
-    elif r1 == 6:
-        print("Its Saturday-5")
-    elif r1 == 0:
-        print("Its Sunday-6")
+r1 = x + (31 * (y-1))
+r2 = (365 * (z-1)) - 1
+result = (r1 + r2)%7
+if result == 0:
+    print("Monday")
+if result == 1:
+    print("Tuesday")
+if result == 2:
+    print("Wednesday")
+if result == 3:
+    print("Thurday")
+if result == 4:
+    print("Friday")
+if result == 5:
+    print("Saturday")
+if result == 6:
+    print("Sunday")
+print(result)
